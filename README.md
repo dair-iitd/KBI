39b9d9b0e9b7114eebf002a0adae2ebe7bfbde1d
MurtyShikhar <shikhar.murty@gmail.com>
2017-10-10 00:52:24 -0400

    change README

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Joint Matrix-Tensor Factorization for Knowledge Base Inference
-Code for our TACL 2017 Submission titled "Joint Matrix-Tensor Factorization for Knowledge Base Inference"
+Code for our TACL 2017 Submission titled "Joint Matrix-Tensor Factorization for Knowledge Base Inference" ([arXiv](https://arxiv.org/pdf/1706.00637.pdf))
 
-Joint Matrix-Tensor Factorization for Knowledge Base Inference, Prachi Jain, Shikhar Murty, Mausam, Soumen Chakrabarti 
-
+Joint Matrix-Tensor Factorization for Knowledge Base Inference, Prachi Jain*, Shikhar Murty*, Mausam, Soumen Chakrabarti 
+* Equal Contribution
 
 # Running instructions
 First clone the repository:
@@ -20,22 +20,24 @@ Also, ensure you have the latest versions of both **Theano** and **numpy**.
 To run the atomic models(where every relation is considered as an atom), for example DistMult, E or TransE, run the following:
 
 ```
-<PRACHI: Please put command here>
+./run.sh <dataset> <model> atomic
 ```
 where MODEL can be **distMult**, **E** , **complex** , **TransE**, DATASET is **wn18**, **fb15k**, **fb15k-237**, **nyt-fb** and OPTIMIZER is
 either **Adagrad** or **RMSprop**
 
-To make the train data again (in the form of matrices), run
+
+To create training data, run
 ```
 dump_data.py 
 ```
 
 and  for creating data for MF, run 
-'''
+```
 dump_data_pairwise.py 
-'''
+```
 
 with the right set of parameters. (For info on parameters, run dump_data.py -h)
 
+For the datasets, please mail me at shikhar.murty@gmail.com. 
 
 
--- run.sh
+++ run.sh
@@ -25,24 +25,8 @@ add_tanh=0
 shared_r=0
 norm_score=0
 loss="ll"
-#model_path="MF_FB15k_dim100.h5"
-#model_path="distMult_1_.._DATA_REPOSITORY_wn18_original_encoded_data_without-text.h5"
-#model_path="adder_fb15k-237_0.1.h5"
-#model_path="adder_fb15k_0.0.h5"
-#model_path="MF_FB15k_dim100.h5"
-#distMult_NYT_FB_dim100.h5"
-model_path="distMult_FB15k_dim100.h5"
+base_dir="/Users/shikhar/Desktop/code/KBI/"
 
-#model_path="FE_fb15k.h5"
-#model_path="distMult_FB15k-237_dim100.h5"
-#"MF_FB15k-237_dim100.h5"
-#"MF_FB15k_dim100.h5"
-
-#model_path="distMult_FB15k_dim100.h5"
-#"distMult_NYT_FB_dim100.h5"
-#"MF_oovtrained_nyt-fb100dim.h5"
-#"MF_WN18_dim100.h5"
-#"distMult_wn18_dim100_maxmargin.h5"
 
 if [[ $# == 3 ]]; then 
     echo "By default run file is train combined models and training is set to false"
@@ -68,6 +52,8 @@ elif [[ $# == 5 ]]; then
         echo "You passed an illegal execution mode"
     fi
     train=$5
+
+
 elif [[ $# == 7 ]]; then
     if [[ $4 == "atomic" ]]; then
         run_file="trainAtomicModels.py"
@@ -79,168 +65,7 @@ elif [[ $# == 7 ]]; then
     train=$5
     l2_entity_pair=$6
     gpu_id=$7
-elif [[ $# == 8 ]]; then
-    if [[ $4 == "atomic" ]]; then
-        run_file="trainAtomicModels.py"
-    elif [[ $4 == "combined" ]]; then
-        run_file="train_combined_model.py"
-    else
-        echo "You passed an illegal execution mode"
-    fi
-    train=$5
-    l2_entity_pair=$6
-    gpu_id=$7
-    num_epochs=$8
-elif [[ $# == 9 ]]; then
-    if [[ $4 == "atomic" ]]; then
-        run_file="trainAtomicModels.py"
-    elif [[ $4 == "combined" ]]; then
-        run_file="train_combined_model.py"
-    else
-        echo "You passed an illegal execution mode"
-    fi
-    train=$5
-    l2_entity_pair=$6
-    gpu_id=$7
-    num_epochs=$8
-    learning_rate=$9
-elif [[ $# == 12 ]]; then
-    if [[ $4 == "atomic" ]]; then
-        run_file="trainAtomicModels.py"
-    elif [[ $4 == "combined" ]]; then
-        run_file="train_combined_model.py"
-    else
-        echo "You passed an illegal execution mode"
-    fi
-    train=$5
-    l2_entity_pair=$6
-    gpu_id=$7
-    num_epochs=$8
-    learning_rate=$9
-    static_alpha=${10}
-    alphaMF=${11}
-    static_beta=${12}
-elif [[ $# == 13 ]]; then
-    if [[ $4 == "atomic" ]]; then
-        run_file="trainAtomicModels.py"
-    elif [[ $4 == "combined" ]]; then
-        run_file="train_combined_model.py"
-    else
-        echo "You passed an illegal execution mode"
-    fi
-    train=$5
-    l2_entity_pair=$6
-    gpu_id=$7
-    num_epochs=$8
-    learning_rate=$9
-    static_alpha=${10}
-    alphaMF=${11}
-    static_beta=${12}
-    unit_norm_reg=${13}	
-elif [[ $# == 14 ]]; then
-    if [[ $4 == "atomic" ]]; then
-        run_file="trainAtomicModels.py"
-    elif [[ $4 == "combined" ]]; then
-        run_file="train_combined_model.py"
-    else
-        echo "You passed an illegal execution mode"
-    fi
-    train=$5
-    l2_entity_pair=$6
-    gpu_id=$7
-    num_epochs=$8
-    learning_rate=$9
-    static_alpha=${10}
-    alphaMF=${11}
-    static_beta=${12}
-    unit_norm_reg=${13}
-    shared_r=${14}
-elif [[ $# == 15 ]]; then
-    if [[ $4 == "atomic" ]]; then
-        run_file="trainAtomicModels.py"
-    elif [[ $4 == "combined" ]]; then
-        run_file="train_combined_model.py"
-    else
-        echo "You passed an illegal execution mode"
-    fi
-    train=$5
-    l2_entity_pair=$6
-    gpu_id=$7
-    num_epochs=$8
-    learning_rate=$9
-    static_alpha=${10}
-    alphaMF=${11}
-    static_beta=${12}
-    unit_norm_reg=${13}
-    shared_r=${14}
-    evalDev=${15}
-elif [[ $# == 16 ]]; then
-    if [[ $4 == "atomic" ]]; then
-        run_file="trainAtomicModels.py"
-    elif [[ $4 == "combined" ]]; then
-        run_file="train_combined_model.py"
-    else
-        echo "You passed an illegal execution mode"
-    fi
-    train=$5
-    l2_entity_pair=$6
-    gpu_id=$7
-    num_epochs=$8
-    learning_rate=$9
-    static_alpha=${10}
-    alphaMF=${11}
-    static_beta=${12}
-    unit_norm_reg=${13}
-    shared_r=${14}
-    evalDev=${15}
-    add_loss=${16}
-elif [[ $# == 18 ]]; then
-    if [[ $4 == "atomic" ]]; then
-        run_file="trainAtomicModels.py"
-    elif [[ $4 == "combined" ]]; then
-        run_file="train_combined_model.py"
-    else
-        echo "You passed an illegal execution mode"
-    fi
-    train=$5
-    l2_entity_pair=$6
-    gpu_id=$7
-    num_epochs=$8
-    learning_rate=$9
-    static_alpha=${10}
-    alphaMF=${11}
-    static_beta=${12}
-    unit_norm_reg=${13}
-    shared_r=${14}
-    evalDev=${15}
-    add_loss=${16}
-    dropout_MF=${17}
-    dropout_DM=${18}
 
-elif [[ $# == 19 ]]; then
-    if [[ $4 == "atomic" ]]; then
-        run_file="trainAtomicModels.py"
-    elif [[ $4 == "combined" ]]; then
-        run_file="train_combined_model.py"
-    else
-        echo "You passed an illegal execution mode"
-    fi
-    train=$5
-    l2_entity_pair=$6
-    gpu_id=$7
-    num_epochs=$8
-    learning_rate=$9
-    static_alpha=${10}
-    alphaMF=${11}
-    static_beta=${12}
-    unit_norm_reg=${13}
-    shared_r=${14}
-    evalDev=${15}
-    add_loss=${16}
-    dropout_MF=${17}
-    dropout_DM=${18}
-    vect_dim=${19}
-fi
 
 dataset=$1
 model=$2
@@ -274,4 +99,4 @@ cmd="date; time THEANO_FLAGS=mode=FAST_RUN,device=gpu$gpu_id,floatX=float32 pyth
 echo $cmd
 
 
-THEANO_FLAGS=mode=FAST_RUN,device=gpu$gpu_id,floatX=float32 python -u $run_file -neg_samples $neg_samples -num_entities $ent -num_relations $rels -num_entity_pairs $ent_pairs  -model $model -dataset ../DATA_REPOSITORY/$dataset/$file_dir -l2_entity $l2 -l2_relation $lr -batch_size $batch_size -epochs $num_epochs -eval_every $eval_every -eval_after $eval_after -train $train -oov_avg $oov_avg -oov_train $oov_train -unit_norm $unit_norm_reg -rate $learning_rate -vect_dim $vect_dim -add_tanh $add_tanh -shared_r $shared_r -norm_score $norm_score -l2_entity_pair $l2_entity_pair -static_alpha $static_alpha -alphaMF $alphaMF -static_beta $static_beta -evalDev $evalDev -l2_relation_MF $l2_entity_pair -add_loss $add_loss -dropout_MF $dropout_MF -dropout_DM $dropout_DM -oov_eval $oov_eval -loss $loss -model_path $model_path &> $3
+THEANO_FLAGS=mode=FAST_RUN,device=gpu$gpu_id,floatX=float32 python -u $run_file -neg_samples $neg_samples -num_entities $ent -num_relations $rels -num_entity_pairs $ent_pairs  -model $model -dataset $base_dir/DATA_REPOSITORY/$dataset/$file_dir -l2_entity $l2 -l2_relation $lr -batch_size $batch_size -epochs $num_epochs -eval_every $eval_every -eval_after $eval_after -train $train -oov_avg $oov_avg -oov_train $oov_train -unit_norm $unit_norm_reg -rate $learning_rate -vect_dim $vect_dim -add_tanh $add_tanh -shared_r $shared_r -norm_score $norm_score -l2_entity_pair $l2_entity_pair -static_alpha $static_alpha -alphaMF $alphaMF -static_beta $static_beta -evalDev $evalDev -l2_relation_MF $l2_entity_pair -add_loss $add_loss -dropout_MF $dropout_MF -dropout_DM $dropout_DM -oov_eval $oov_eval -loss $loss -model_path $model_path &> $3
